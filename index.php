a6d368f3feead7b3e5f28c6e76c139c3320382a6
Ananda Novaldo <macbookair@MacBooks-MacBook-Air.local>
2025-02-01 02:25:50 +0700

    Menambahkan paragraf baru di file index.php

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
     <body>
         <h1> Hello World! </h1>
         <p> Saya sedang mempelajari git dan github bersama dosen saya pak sandhika galih </p>
+        <p> Saya sedang berusaha konsisten mempelajari ilmu tentang program untuk masa depan yang lebih baik </p>
     </body>
     </html>
